9606ff5404b50fa5761e6a1d55844bbd6eefde74
Nico Yañez <airtonicolasy1996@gmail.com>
2022-02-19 21:11:11 -0300

    Termine estilos del formulario de creacion de actividades

--- client/src/component/Form/ActivityCreate.jsx
+++ client/src/component/Form/ActivityCreate.jsx
@@ -8,6 +8,44 @@ const ActivityCreateForm = () => {
       <Nav />
       <div className={style.form}>
         <h1>Create Activity</h1>
+        <form>
+          <input
+            type="text"
+            className={style.controls}
+            name=""
+            id=""
+            placeholder="Nombre de la actividad a crear"
+          />
+          <input
+            type="text"
+            className={style.controls}
+            name=""
+            id=""
+            placeholder="Dificultad de la actividad"
+          />
+          <input
+            type="text"
+            className={style.controls}
+            name=""
+            id=""
+            placeholder="Duracion de la actividad"
+          />
+          <input
+            type="text"
+            className={style.controls}
+            name=""
+            id=""
+            placeholder="Estacion en la que se realiza la actividad"
+          />
+          <input
+            type="text"
+            className={style.controls}
+            name=""
+            id=""
+            placeholder="Pais/es donde se encuentra la actividad"
+          />
+          <input type="submit" className={style.botons} value="Create" />
+        </form>
       </div>
     </div>
   );
